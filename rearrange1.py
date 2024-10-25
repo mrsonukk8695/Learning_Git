278e583c1734e61c1dd24d50cc04f6f80eb15879
mrsonukk8695 <mrsonukk8695@gmail.com>
2024-10-25 19:25:12 +0530

    Update rearrange1.py
    Added a Comment for better understanding of code

--- rearrange1.py
+++ rearrange1.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# Adding a line that tells about the changes and the work that this files from which users can understand what this code perform easily
 import re
 def rearrange_name(name):
     result = re.search(r"^([\w .-]*), ([\w .-]*)$", name)
